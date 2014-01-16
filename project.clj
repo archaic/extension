ef74f7f9a1989189da2f770253147d069f88f8ba
Christopher Rosengren <christopher.rosengren@gmail.com>
2014-01-16 23:23:28 +1100

    Update

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
-(defproject extension "0.2.0"
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+(defproject extension "0.2.1"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.datomic/datomic-free "0.8.4254"]]
   :description "Extensions to Clojure"
   :profiles {:dev {:dependencies [[midje "1.5.1"]]}})
