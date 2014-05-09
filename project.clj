30db917c6610e661d2343cf653a9739d26107acf
Christopher Rosengren <christopher.rosengren@gmail.com>
2014-05-09 22:37:27 +1000

    Update version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject extension "0.2.3"
+(defproject extension "0.2.4"
   :dependencies [[com.datomic/datomic-free "0.9.4707"]
                  [org.clojure/clojure "1.6.0"]]
   :description "Extensions to Clojure")
