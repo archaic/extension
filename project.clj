95db3bdd1210f34a03cf5520c3b7f263ba38e05f
Christopher Rosengren <christopher.rosengren@gmail.com>
2014-05-11 22:16:52 +1000

    Remove datomic dependency

--- project.clj
+++ project.clj
@@ -1,4 +1,3 @@
-(defproject extension "0.2.4"
-  :dependencies [[com.datomic/datomic-free "0.9.4707"]
-                 [org.clojure/clojure "1.6.0"]]
+(defproject extension "0.2.5"
+  :dependencies [[org.clojure/clojure "1.6.0"]]
   :description "Extensions to Clojure")
--- src/extension/collection.clj
+++ src/extension/collection.clj
@@ -5,3 +5,21 @@
   "nth order differencing on a collection of numbers"
   [xs n]
   (map #(- %2 %) xs (drop n xs)))
+
+(defn chain?
+  "A chain is a sequence of consecutive integers, returns true if
+   xs has a chain of length n (or longer)"
+  [xs n]
+  (loop [last-integer nil
+         chain-length 0
+         xs xs]
+    (let [i (first xs)
+          xs (rest xs)]
+      (if (< chain-length n)
+        (when i
+          (recur i
+                 (if (= (dec i) last-integer)
+                   (inc chain-length)
+                   1)
+                 xs))
+        true))))
