5e07dd9874d87cab2fdea802c35c467b93734855
Christopher Rosengren <christopher.rosengren@gmail.com>
2015-04-11 22:48:47 +1000

    Update dependencies, Version 0.3.3

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject com.eoneq/extension "0.3.2"
-  :dependencies [[org.clojure/clojure "1.7.0-alpha5"]]
+(defproject com.eoneq/extension "0.3.3"
+  :dependencies [[org.clojure/clojure "1.7.0-beta1"]]
   :description "Extensions to Clojure")
