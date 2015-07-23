6e5d7b36bc3b417cfd49f50edd02462347f6db3e
Christopher Rosengren <christopher.rosengren@gmail.com>
2015-07-23 21:06:52 +1000

    Update

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject com.eoneq/extension "0.3.3"
-  :dependencies [[org.clojure/clojure "1.7.0-beta1"]]
+(defproject com.eoneq/extension "0.3.6"
+  :dependencies [[org.clojure/clojure "1.7.0"]]
   :description "Extensions to Clojure")
--- src/extension/math.clj
+++ src/extension/math.clj
@@ -9,9 +9,30 @@
       0.0)))
 
 (defn sd
-  [xs]
-  (let [n (count xs)]
-    (if (pos? n)
-      (let [mean (mean xs)]
-        (Math/sqrt (/ (reduce + (map (fn [x] (Math/pow (- x mean) 2)) xs)) n)))
-      0.0)))
+  ([xs]
+   (sd xs (mean xs)))
+  ([xs mean]
+   (let [n (count xs)]
+     (if (pos? n)
+       (Math/sqrt (/ (reduce + (map (fn [x] (Math/pow (- x mean) 2)) xs)) n))
+       0.0))))
+
+(defn ceil-50
+  "Rounds amount down to the nearest 50 cents"
+  [x]
+  (let [characteristic (int x)
+        mantissa (- x characteristic)
+        eps 1e-6]
+
+    (cond (< 0 mantissa eps) characteristic
+          (< 0 mantissa (+ 0.5 eps)) (+ characteristic 0.5)
+          :else (Math/ceil x))))
+
+(defn ceil
+  [number]
+  (Math/ceil number))
+
+(defn floor-10
+  "Rounds amount down to the nearest 10 cents"
+  [x]
+  (/ (Math/floor (* 10 x)) 10))
