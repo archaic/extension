750e2e455a5834a809f3fa7daec07e39c1329354
Christopher Rosengren <christopher.rosengren@gmail.com>
2016-02-15 11:14:48 +1100

    Handle vulgar fractions

--- src/extension/string.clj
+++ src/extension/string.clj
@@ -1,4 +1,6 @@
-(ns extension.string)
+(ns extension.string
+  (:require [clojure.string :as s])
+  (:import [java.text Normalizer Normalizer$Form]))
 
 (defn ->f
   [s]
@@ -11,3 +13,15 @@
   (when (string? s)
     (when-let [s (re-find #"\d+" s)]
       (Integer/parseInt s))))
+
+(defn parse-vulgar-fraction
+  [s]
+  (let [[numerator denominator]
+        (-> s
+            (Normalizer/normalize Normalizer$Form/NFKD)
+            (s/split #"\u2044"))]
+    (/ (Integer/parseInt numerator)
+       (Integer/parseInt denominator))))
+
+(def re-vulgar-fraction
+  #"[\u2150-\u215F]|[\u00BC-\u00BE]")
