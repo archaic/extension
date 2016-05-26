a871b13b32c9dcf53524d3745ba132ab46d14303
Christopher Rosengren <christopher.rosengren@gmail.com>
2016-05-27 00:47:01 +1000

    Reflection warnings in math

--- src/extension/math.clj
+++ src/extension/math.clj
@@ -10,22 +10,39 @@
 
 (defn sd
   ([xs]
-   (sd xs (mean xs)))
+   (sd xs
+       (mean xs)))
+  
   ([xs mean]
    (let [n (count xs)]
      (if (pos? n)
-       (Math/sqrt (/ (reduce + (map (fn [x] (Math/pow (- x mean) 2)) xs)) n))
+       
+       (Math/sqrt (/ (reduce +
+                             (map (fn [x]
+                                    (Math/pow (- x mean)
+                                              2))
+                                  xs))
+                     n))
        0.0))))
 
 (defn ceil-50
   "Rounds amount down to the nearest 50 cents"
   [x]
-  (let [characteristic (int x)
-        mantissa (- x characteristic)
+  (let [characteristic
+        (int x)
+        
+        mantissa
+        (- x characteristic)
+        
         eps 1e-6]
 
-    (cond (< 0 mantissa eps) characteristic
-          (< 0 mantissa (+ 0.5 eps)) (+ characteristic 0.5)
+    (cond (< 0 mantissa eps)
+          characteristic
+          
+          (< 0 mantissa
+             (+ 0.5 eps))
+          (+ characteristic 0.5)
+          
           :else (Math/ceil x))))
 
 (defn ceil
@@ -35,8 +52,13 @@
 (defn floor-10
   "Rounds amount down to the nearest 10 cents"
   [x]
-  (/ (Math/floor (* 10 x)) 10))
+  (/ (Math/floor (* 10
+                    x))
+     10))
 
 (defn roughly?
   [x y tol]
-  (< (Math/abs (- x y)) tol))
+  (let [^Double difference
+        (- x y)]
+    (< (Math/abs difference)
+       tol)))
