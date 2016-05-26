be8a56e598e2c55a000359ade0de37aaa78cfbbd
Christopher Rosengren <christopher.rosengren@gmail.com>
2016-05-27 00:47:13 +1000

    Update version

--- project.clj
+++ project.clj
@@ -1,3 +1,7 @@
-(defproject com.eoneq/extension "0.3.11"
+(defproject com.eoneq/extension "0.3.12"
+
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :description "Extensions to Clojure")
+  
+  :description "Extensions to Clojure"
+
+  :global-vars {*warn-on-reflection* true})
