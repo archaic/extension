55172cfb09cc25bf8cb72615d1236151ff7976d9
Christopher Rosengren <christopher.rosengren@gmail.com>
2020-02-14 18:29:10 +1100

    update

--- src/extension/math.clj
+++ src/extension/math.clj
@@ -104,3 +104,81 @@
                  (when (= v mode-value)
                    k))
                data))))
+
+
+(defn interquartile-range
+  [coll]
+  (when (seq coll)
+    (let [sorted (sort coll)
+          cnt (count sorted)
+          halfway (quot cnt 2)
+          q1 (median (take halfway sorted))
+          q2 (median (take-last halfway sorted))]
+      {:q1 q1
+       :q2 q2})))
+
+(defn skewness
+  [xs]
+
+  (let [m
+        (mean xs)
+
+        s
+        (sd xs)
+
+        n
+        (count xs)
+
+        numerator
+        (reduce + (map (fn [x]
+                         (Math/pow (- x m)
+                                   3))
+                       xs))
+
+        denominator
+        (* (- n 1)
+           (Math/pow s 3))]
+
+    (/ numerator
+       denominator)))
+
+(defn kurtosis
+  [xs]
+
+  (let [m
+        (mean xs)
+
+        s
+        (sd xs)
+
+        n
+        (count xs)
+        
+        m4
+        (/ (reduce +
+                   (map (fn [x]
+                          (Math/pow (- x m) 4))
+                        xs))
+           n)]
+    (when (< 0 s)
+      (/ m4
+         (Math/pow s 4)))))
+
+
+(defn jarque-bera
+  [xs]
+
+  (let [n
+        (count xs)
+
+        s
+        (skewness xs)
+
+        k
+        (kurtosis xs)]
+
+    (* (/ n 6)
+       (+ (Math/pow s 2)
+          (* 0.25
+             (Math/pow (- k 3)
+                       2))))))
