2479acfe9acb66c16ab620617e2043bc13a77e41
Christopher Rosengren <christopher.rosengren@gmail.com>
2020-09-18 16:28:47 +1000

    update

--- src/extension/table.clj
+++ src/extension/table.clj
@@ -60,3 +60,56 @@
    (print-table (keys (first rows))
                 rows)))
 
+(defn print-basic-table
+  ([ks rows]
+   (when (seq rows)
+     
+     (let [widths
+           (map (fn [k]
+                  (apply max
+                         (+ 2 (count (str k)))
+                         (map #(+ 3 (count (str (get % k))))
+                              rows)))
+                ks)
+           
+           fmts
+           (map #(str "%" % "s")
+                widths)
+
+           fmt-header
+           (fn [row]
+             (s/join " "
+                     (map (fn [k fmt]
+
+                            (let [data
+                                  (get row k)
+
+                                  data
+                                  (if (keyword? data)
+                                    (name data)
+                                    data)]
+
+                              (format fmt
+                                      (str " "
+                                           (s/capitalize data)
+                                           " "))))
+                          ks
+                          fmts)))
+           
+           fmt-row
+           (fn [row]
+             (s/join " "
+                     (map (fn [k fmt]
+                            (format fmt
+                                    (get row k)))
+                          ks
+                          fmts)))]
+       
+       (println (fmt-header (zipmap ks ks)))
+       
+       (doseq [row rows]
+         (println (fmt-row row))))))
+  
+  ([rows]
+   (print-table (keys (first rows))
+                rows)))
